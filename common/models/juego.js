d816f58606f1c6aa5856e904406f626e3a80cd89
Facundo Jadur <facundojadur97@gmail.com>
2018-10-08 09:38:16 +0200

    Remote-method getNombre de Juego creado

--- common/models/juego.js
+++ common/models/juego.js
@@ -2,4 +2,20 @@
 
 module.exports = function(Juego) {
 
+/**
+ * Devuelve el nombre de un juego.
+ * @param {Function(Error, string)} callback
+ */
+
+Juego.prototype.getNombre = function(callback) {
+	var juego=this;
+
+  callback(null, juego.nombre);
+};
+
+
+
+
+
+//finale
 };
